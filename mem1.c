9bc875255817b15f35140b86cd822544bd6415d9
Wael Al Masri <waelalmasri@Waels-MacBook-Pro.local>
2021-04-20 18:40:17 +0200

    shared mem example

--- mem1.c
+++ mem1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <string.h>
+#include <unistd.h>
 
 
 
@@ -16,4 +18,26 @@ void* create_shared_memory(size_t size) {
   // The remaining parameters to `mmap()` are not important for this use case,
   // but the manpage for `mmap` explains their purpose.
   return mmap(NULL, size, protection, visibility, -1, 0);
+}
+
+int main() {
+  char parent_message[] = "hello";  // parent process will write this message
+  char child_message[] = "goodbye"; // child process will then write this one
+
+  void* shmem = create_shared_memory(128);
+
+  memcpy(shmem, parent_message, sizeof(parent_message));
+
+  int pid = fork();
+
+  if (pid == 0) {
+    printf("Child read: %s\n", shmem);
+    memcpy(shmem, child_message, sizeof(child_message));
+    printf("Child wrote: %s\n", shmem);
+
+  } else {
+    printf("Parent read: %s\n", shmem);
+    sleep(1);
+    printf("After 1s, parent read: %s\n", shmem);
+  }
 }
