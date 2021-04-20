bdd005fd1b9664bcd479e874b773b9937b8da596
Wael Al Masri <waelalmasri@Waels-MacBook-Pro.local>
2021-04-20 20:45:48 +0200

    testing gpio with c

--- rasp1.c
+++ rasp1.c
@@ -1,35 +1,78 @@
-#include <sys/time.h>
-int main(int argc, char **argv)
-{
-int distance;
-struct timeval start,end;;
-double elapsed ;
-// Set up gpi pointer for direct register access
-setup_io();
-INP_GPIO(23); // must use INP_GPIO before we can use OUT_GPIO
-OUT_GPIO(23);
-INP_GPIO(7); //input for echo pin
+/****
+* gpio-read.c -- example program for reading gpio values using the <linux/gpio.h> C API
+* The API consists primarily of data structures used as parameters to system calls.
+* Adapted from from a youtube video of Linus Walleij at the Embedded Linux Conference Europe
+* GPIO for Engineers and Makers: starting at timestamp 44:11
+* For simplicity, there is no error checking.
+****/
 
-GPIO_CLR = (1 << 23);
-usleep(30000) // allow module to settle
-
-printf("starting measurement")
-GPIO_SET = (1<<23);
-usleep(10); // send pulse 10us 
-GPIO_CLR = (1<<23);
+//Need the gpio API and support for the system calls.
+#include <linux/gpio.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <unistd.h>
 
 
-while ((GPIO_CLR == ( 0<<7)))
-{} 
-gettimeofday(&start,NULL);
-while ((GPIO_SET == ( 1<<7))) 
-{} // input HIGH
-gettimeofday(&end,NULL);
-elapsed = (end.tv_sec - start.tv_sec);
-distance = (elapsed)/2 ; //in cm
-printf("distance: %d cm¥n", distance);
+int main(){
 
+
+//Opening device and getting file descriptor.
+int fd = open("/dev/gpiochip0",O_RDONLY);
+
+//structure for holding chip information
+//This structure is defined in /usr/include/linux/gpio.h
+struct gpiochip_info cinfo;
+
+//Getting the chip information via the ioctl system call
+//GPIO_GET_CHIPINFO_IOCTL defined also in /usr/include/linux/gpio.h
+int ret = ioctl(fd,GPIO_GET_CHIPINFO_IOCTL,&cinfo);
+
+//print out the chip information
+fprintf(stdout, "GPIO chip: %s, \"%s\", %u GPIO lines\n",
+    cinfo.name, cinfo.label, cinfo.lines);
+
+//structure for holding line information.
+//structure defined in /usr/include/linux/gpio.h
+struct gpioline_info linfo;
+
+//get generic line information from system call
+ret = ioctl(fd,GPIO_GET_LINEINFO_IOCTL, &linfo);
+
+//Not sure what this line_offset is, but we specify the gpio number later.
+fprintf(stdout,"line %2d: %s\n",linfo.line_offset,linfo.name);
+
+//Reading lines
+//Set up some handles for requests and data
+struct gpiohandle_request req;
+struct gpiohandle_data data;
+
+//Although req and data can read multiple gpios at a time, we'll use just one
+//This reads line offset 4, which corresponds to the BCM value in "gpio readall"
+req.lineoffsets[0] = 22;
+//have to indicate how many lines we are reading.
+req.lines = 1;
+//Make this an input request
+req.flags = GPIOHANDLE_REQUEST_INPUT;
+
+//Optionally give the line a name
+strcpy(req.consumer_label, "First Switch");
+
+//Get a line handle. Note that req.fd is provided a handle for the next ioctl. 
+int lhfd = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
+
+//Loop with some delay that keeps checking the input and displaying the value
+for(int ii = 0; ii < 1000; ++ii){
+
+    ret = ioctl(req.fd,  GPIOHANDLE_GET_LINE_VALUES_IOCTL, &data);
+
+    printf("line %d is %s\n",req.lineoffsets[0], data.values[0] ? "high" : "low");
+    //wait 0.25 seconds
+    usleep(250000);
+}
 
 return 0;
-
 }
