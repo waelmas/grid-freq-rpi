b4c3625e3d349feeb1eaf0e79787840d8cd27b1a
Wael Al Masri <waelalmasri@Waels-MacBook-Pro.local>
2021-04-21 15:07:31 +0200

    Reading high/low from pin 15 working

--- rasp2.c
+++ rasp2.c
@@ -166,7 +166,7 @@ while(1){
             // printf("line %d is %s\n",req.lineoffsets[0], data.values[0] ? "high" : "low");
             //wait 0.25 seconds
             // usleep(250000);
-            child_message_base = data.values[0] ? child_message1 : child_message0;
+            child_message_base = (data.values[0] ? child_message1 : child_message0);
             printf(child_message_base);
             // printf(data.values[0]);
             // memcpy(shmem, child_message_base, sizeof(child_message_base));
